66417292893bcdb4e719a251c8ba38bbc5affb24
Vladimir Kremlev <vkremlev@vento.ru>
2020-02-07 15:06:37 +0300

    Updates

---
+++ precache-manifest.9e04e5a41a9e1c20335602ff8e04f722.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a423a1c8c9e7b5864db762ae39ae16c9",
+    "url": "/Morrone/index.html"
+  },
+  {
+    "revision": "87eba05c5cfade1ec1b5",
+    "url": "/Morrone/static/css/3.33c00429.chunk.css"
+  },
+  {
+    "revision": "63d14e9844d62b5e69e1",
+    "url": "/Morrone/static/css/4.d89c5a72.chunk.css"
+  },
+  {
+    "revision": "bd0f939070266e4ec45c",
+    "url": "/Morrone/static/css/main.36193e7a.chunk.css"
+  },
+  {
+    "revision": "55627727adf764cb4d3c",
+    "url": "/Morrone/static/js/2.5216a1e3.chunk.js"
+  },
+  {
+    "revision": "87eba05c5cfade1ec1b5",
+    "url": "/Morrone/static/js/3.a59e9a0c.chunk.js"
+  },
+  {
+    "revision": "63d14e9844d62b5e69e1",
+    "url": "/Morrone/static/js/4.081f3a4e.chunk.js"
+  },
+  {
+    "revision": "bd0f939070266e4ec45c",
+    "url": "/Morrone/static/js/main.0ee03732.chunk.js"
+  },
+  {
+    "revision": "b02ad177590ba0e9d8d9",
+    "url": "/Morrone/static/js/runtime~main.bac19d99.js"
+  }
+]);
